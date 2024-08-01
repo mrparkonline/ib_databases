eb72ec33ad86221a6173e6046470758d3d8e435d
Mr. Park (Windows) <mrparkonline@gmail.com>
2024-08-01 11:26:13 -0400

    fixed the lesson1 initialization and its table

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -9,4 +9,16 @@ connection = sqlite3.connect("pokemon.db")
 # Step 3: create a cursor that helps us to communicate with a the database
 cursor = connection.cursor()
 
-# Step 4: execute SQL queries with a method called ".execute()"
+# Step 4: execute SQL queries with a method called ".execute()" from the cursor
+
+# QUERIES GO HERE
+
+
+# Step 5: Commit your changes on the database
+connection.commit()
+
+# Step 6: Optional, see the total number of changes
+print(f"Current Session's Number of changes: {connection.total_changes}")
+
+# Step 7: When finished with the database, close your connection
+connection.close()
