d71120ae1d856229a877901d882c1383b46881c3
J Park <mrparkonline@gmail.com>
2024-08-02 12:53:11 -0400

    added lesson 2 content

---
+++ lesson2/lesson2.py
@@ -0,0 +1,18 @@
+# Python + SQLite Lesson 2
+
+# Step 1: import SQLite3 Module
+import sqlite3
+
+# Step 2: create a connection with a SQL database file
+connection = sqlite3.connect("swim.db")
+
+# Step 3: create a cursor that helps us to communicate with a the database
+cursor = connection.cursor()
+
+# Step 4: execute SQL queries with a method called ".execute()" from the cursor
+
+# QUERIES GO HERE
+
+# Step 5: when finished with the database, close your cursor and connection
+cursor.close()
+connection.close()
